f04096594b38520a8b9a2787ec3f0780a5ded6a6
Steve Pryde <steve@stevepryde.com>
2021-12-08 10:37:47 +1030

    Add chrome options example

---
+++ examples/chrome_options.rs
@@ -0,0 +1,36 @@
+//! Requires chromedriver running on port 4444:
+//!
+//!     chromedriver --port=4444
+//!
+//! Run as follows:
+//!
+//!     cargo run --example chrome_options
+
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let mut caps = DesiredCapabilities::chrome();
+    caps.add_chrome_option(
+        "prefs",
+        serde_json::json!({
+            "profile.default_content_settings": {
+                "images": 2
+            },
+            "profile.managed_default_content_settings": {
+                "images": 2
+            }
+        }),
+    )?;
+    let driver = WebDriver::new("http://localhost:4444", &caps).await?;
+
+    // Navigate to https://wikipedia.org.
+    driver.get("https://wikipedia.org").await?;
+
+    // Always explicitly close the browser. There are no async destructors.
+    driver.quit().await?;
+
+    Ok(())
+}
